bc37a87733720b6287804fe397e1cbd02607dba6
Adileelh <adile.elhaddik@gmail.com>
2023-05-13 15:27:22 +0200

    line too long correction

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,18 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/schema/',
+         SpectacularAPIView.as_view(),
+         name='api-schema'),
     # Optional UI:
     path('api/docs/',
-         SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs'),
     path('api/redoc/',
-         SpectacularRedocView.as_view(url_name='api-schema'), name='api-redoc'),
+         SpectacularRedocView.as_view(url_name='api-schema'),
+         name='api-redoc'),
     path('api/user/', include('user.urls')),
 ]
